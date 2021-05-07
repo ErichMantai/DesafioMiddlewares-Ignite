d4e4daedf0e70499597c759ffb3512e064236fc4
erickmantai <erickmantai@gmail.com>
2021-05-06 21:05:45 -0300

    challenge concluded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, v4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(
+    user => user.username == username
+  );
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  if (user.todos.length >= 10 && user.pro === false) {
+    return response.status(403).json({ error: "your plan allows you to generate only 10 tasks" })
+  }
+
+  if (user.todos.length < 10 && user.pro === false) {
+    return next();
+  }
+
+  if (user.pro === true) {
+    return next();
+  }
+
 }
 
+
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username)
+  if (!user) return response.status(404).json({ error: 'User not found' })
+
+
+  const uIdIsvaid = validate(id);
+
+  if (!uIdIsvaid) {
+    return response.status(400).json({ error: "Id is not a valid" });
+  }
+
+
+  const todoUser = user.todos.find(todo => todo.id === id);
+
+  if (!todoUser) {
+    return response.status(404).json({ error: "task not found" });
+  }
+
+  request.user = user;
+  request.todo = todoUser;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.id === id);
+
+  if (!userExists) return response.status(404).json({ error: "User not found" });
+
+
+  request.user = userExists;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -55,7 +115,7 @@ app.get('/users/:id', findUserById, (request, response) => {
 
 app.patch('/users/:id/pro', findUserById, (request, response) => {
   const { user } = request;
-
+  console.log(user);
   if (user.pro) {
     return response.status(400).json({ error: 'Pro plan is already activated.' });
   }
